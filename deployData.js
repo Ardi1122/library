2d61b74196f72d7d96352a3687a5ed2651fb1e78
Ardi <ardi722308@gmail.com>
2024-10-10 12:21:55 +0800

    update for deployData

--- deployData.js
+++ deployData.js
@@ -6,10 +6,10 @@
   NetlifyDeployUrl: berisi string url Netlify deployment
 */
 
-const studentName = 'TODO: replace me'; // TODO: replace this
+const studentName = 'TAUFIQ'; // TODO: replace this
 
-const studentId = 'TODO: replace me'; // TODO: replace this
+const studentId = 'FS9545151'; // TODO: replace this
 
-const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+const NetlifyDeployUrl = 'https://classy-cobbler-955d93.netlify.app/'; // TODO: replace this
 
 module.exports = { studentName, studentId, NetlifyDeployUrl };
